d8befb4b7186df67cef64e5de6787e97dcf1b74f
bruhamro95 <bruhamro95@gmail.com>
2026-09-12 00:36:47 +0300

    Load academic progress exclusion

--- src/main.jsx
+++ src/main.jsx
@@ -2,6 +2,7 @@ import "./audio-unlock.js";
 import "./mobile-task-search.js";
 import "./tracker-sort-filter.js";
 import "./tracker-bulk-actions.js";
+import "./tracker-progress-exclusion.js";
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
