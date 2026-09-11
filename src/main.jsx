0d2d23efdfef9a065fbf01ef4af3ffd7d39dc47b
bruhamro95 <bruhamro95@gmail.com>
2026-09-12 01:25:16 +0300

    Wire chapter options helper

--- src/main.jsx
+++ src/main.jsx
@@ -2,6 +2,8 @@ import "./audio-unlock.js";
 import "./mobile-task-search.js";
 import "./tracker-sort-filter.js";
 import "./tracker-bulk-actions.js";
+import "./tracker-chapter-options.js";
+import "./audio-unlock.js";
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
