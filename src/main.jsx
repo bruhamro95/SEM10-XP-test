7eef1d65ac9cae076d45afaede71d621bda90754
bruhamro95 <bruhamro95@gmail.com>
2026-09-11 20:30:14 +0300

    Load tracker bulk check enhancement

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,7 @@
 import "./audio-unlock.js";
 import "./mobile-task-search.js";
 import "./tracker-sort-filter.js";
+import "./tracker-bulk-actions.js";
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
