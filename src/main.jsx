99b4adeea196803496404e08034d37a08e210a15
bruhamro95 <bruhamro95@gmail.com>
2026-09-12 01:25:01 +0300

    Wire chapter options helper

--- src/main.jsx
+++ src/main.jsx
@@ -4,6 +4,7 @@ import "./tracker-sort-filter.js";
 import "./tracker-bulk-actions.js";
 import "./tracker-progress-exclusion.js";
 import "./taskbar-boundary.js";
+import "./tracker-chapter-options.js";
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
