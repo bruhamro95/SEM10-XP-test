374052775061fc535058dec148a2c81c358e2f66
bruhamro95 <bruhamro95@gmail.com>
2026-09-12 00:50:47 +0300

    Wire taskbar boundary fix

--- src/main.jsx
+++ src/main.jsx
@@ -3,6 +3,7 @@ import "./mobile-task-search.js";
 import "./tracker-sort-filter.js";
 import "./tracker-bulk-actions.js";
 import "./tracker-progress-exclusion.js";
+import "./taskbar-boundary.js";
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
